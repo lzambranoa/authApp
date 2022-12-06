44b1f2c6d17462151c59e6b703c4617dc8ca60f0
lzamranoa <lzambranoa.software@gmail.com>
2022-12-06 11:58:03 -0500

    uso del formularios reactivos

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -9,6 +9,14 @@ import { FormGroup, FormBuilder, Validators} from '@angular/forms'
 })
 export class LoginComponent {
 
+  /*Para el mnanejo de formularios reactivos es importante primero hacer la importacion
+  en el module de ReactiveFormModules
+  
+  **El uso del FormGroup me permiet hacer validaciones y la  variable miFormulario debe ser 
+  inyectada en nuestro html
+  
+  ** se debe inyectar el formBuilder en el constructor para hacer el suo del servicio*/
+
   miFormulario: FormGroup = this.fb.group({
     email: ['test1@test.com', [Validators.required, Validators.email]],
     password: ['123456', [Validators.required, Validators.minLength(6)]],
