2917f8a9be43477d7bb01a065fec96358a241d18
lzamranoa <lzambranoa.software@gmail.com>
2022-12-12 14:06:23 -0500

    configuración del login, manejo mensajes de erro, carga del token al locaStorage

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormBuilder, Validators} from '@angular/forms'
 import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -24,7 +26,8 @@ export class LoginComponent {
   })
 
   constructor(  private fb: FormBuilder,
-                private authService: AuthService) { }
+                private authService: AuthService,
+                private router: Router) { }
 
   login(){
     console.log(this.miFormulario.value);
@@ -32,8 +35,13 @@ export class LoginComponent {
     const { email, password } = this.miFormulario.value
 
     this.authService.login( email, password )
-      .subscribe(resp => {
-        console.log(resp)
+      .subscribe(ok => {
+        console.log(ok)
+        if(ok === true) {
+          this.router.navigateByUrl('/dashboard');
+        } else {
+          Swal.fire('Error', ok, 'error');
+        }
       })
   }
 
--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,7 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, map, of, tap } from 'rxjs';
 import { environment } from '../../../environments/environment';
-import { AuthResponse } from '../interfaces/interfaces';
+import { AuthResponse, Usuario } from '../interfaces/interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,11 @@ import { AuthResponse } from '../interfaces/interfaces';
 export class AuthService {
 
   private baseUrl: string = environment.baseUrl;
+  private _usuario!: Usuario;
+
+  get usuario() {
+    return { ...this._usuario };
+  }
 
   constructor(private http: HttpClient) { }
 
@@ -18,6 +24,34 @@ export class AuthService {
     const url = `${this.baseUrl}/auth`;
     const body = { email, password };
 
-    return this.http.post<AuthResponse>(url, body);
+    return this.http.post<AuthResponse>(url, body)
+      .pipe(
+        // el operador tap() obliga a que se ejecute primero el código que este dentro antes de siga la ejecucción 
+        // de los demas operadores
+        tap( resp => {
+          if(resp.ok){
+            localStorage.setItem('token', resp.token!)
+            this._usuario = {
+              name: resp.name!,
+              uid: resp.uid!
+            }
+          }
+        }),
+        // !importante el orden de los operadores de rxjs es muy imporatnte ya que se ejecutan de esa forma
+        //Map es un operador de rxjs que me permite mutar la respuesta acondicionandola a lo que se requiere
+        map( resp => resp.ok),
+        //el operador cathError me permite atrapar un error y con la funcion of() me permite
+        //convertir la respuesta en un observable
+        catchError( err => of(err.error.msg))
+      )
+  }
+
+  validarToken() {
+    const url = `${this.baseUrl}/auth/renew`;
+    const headers = new HttpHeaders()
+      .set('x-token', localStorage.getItem('token') || '')
+
+      // los headers como objeto me permite generar los token que se necesiten
+    this.http.get(url, {headers});
   }
 }
